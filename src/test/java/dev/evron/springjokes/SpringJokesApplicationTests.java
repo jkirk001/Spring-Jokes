85cf86be90b495b78ed97753e854b33dcc0bd10e
paylinesales <jone.kirkpatrick@gmail.com>
2021-10-17 16:38:55 -0700

    first commit

---
+++ src/test/java/dev/evron/springjokes/SpringJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.evron.springjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
